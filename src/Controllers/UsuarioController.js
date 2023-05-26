2596ff24decfb9da7579824c7efd50dc3bef5569
Pedro Alonso <112452496+drinhope@users.noreply.github.com>
2023-05-26 18:52:09 -0300

    Aula 7 DONE

--- src/Controllers/UsuarioController.js
+++ src/Controllers/UsuarioController.js
@@ -5,18 +5,28 @@ class UsuarioController{
     async create(req, res){
         const usuario = await UsuarioModel.create(req.body);
 
-        return res.status(200).jason(usuario);
+        return res.status(200).json(usuario);
     }
 
-    read(req, res){
+    async read(req, res){
+        const usuarios = await UsuarioModel.find();
 
+        return res.status(200).json(usuarios);
     }
 
-    update(req, res){
+    async update(req, res){
+        const {id} = req.params;
 
+        const usuario = await UsuarioModel.findByIdAndUpdate(id, req.body, {new : true});
+        return res.status(200).json(usuario);
     }
 
-    delete(req, res){
+    async delete(req, res){
+        const {id} = req.params;
+
+        await UsuarioModel.findByIdAndDelete(id);
+
+        return res.status(200).json({"mensagem": "Usuário deletado com sucesso!"});
 
     }
 }
